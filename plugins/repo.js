c755d13281cab2448cd9abe25e43907daf1fbcc4
maxdila456 <maxdila301@gmail.com>
2025-01-09 23:25:30 +0530

    🍁 𝙎𝙇 DILA 𝐌𝐃🍁

---
+++ plugins/repo.js
@@ -0,0 +1,38 @@
+const { cmd, commands } = require('../command');
+const config = require('../config');
+// repo info
+cmd({
+    pattern: "repo",
+    alias: ["sc", "script", "info"],
+    desc: "Info about the bot repository",
+    category: "main",
+    react: "👨‍💻",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let dec = `*Hello there KUKKU PANDA User! 👋🏻* 
+
+> Simple , Straight Forward But Loaded With Features 🎊, Meet PANDA WhatsApp Bot.
+
+*Thanks for using KUKKU PANDA 🐼💖* 
+
+> Don't forget to frok the repo ⤵️
+
+https://github.com/SHANUWA-ID-MD/MD`;
+
+        await conn.sendMessage(from, { image: { url: `https://files.catbox.moe/x3bdmi.jpg` }, caption: dec, contextInfo: { mentionedJid: [m.sender], forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363354023106228@newsletter', newsletterName: 'JawadTechX', serverMessageId: 143 } } }, { quoted: mek });
+
+        // Send audio
+        await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/repo.m4a' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+
